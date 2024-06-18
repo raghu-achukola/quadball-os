fdaf276d6bfff67ea2db7bef34f9c4eb8a821bff
raghu-achukola <raghu.achukola@gmail.com>
2024-06-18 18:09:34 -0500

    Add AWS Lambda packaging support

---
+++ shell/package-external-lambda-layer.sh
@@ -0,0 +1,6 @@
+mkdir python
+pip install -r ${2}/requirements.txt -t python/
+zip -r ${1}-layer.zip python/
+aws s3 cp ${1}-layer.zip s3://quadball-os-packages/${1}.zip
+rm -r python
+rm ${1}-layer.zip
