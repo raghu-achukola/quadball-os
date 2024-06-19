7cfc151d08353ec5a532f4a97a28eeaa523b4bb1
raghu-achukola <raghu.achukola@gmail.com>
2024-06-18 19:30:01 -0500

    at this point just shoot me rd. 2

--- shell/package-external-lambda-layer.sh
+++ shell/package-external-lambda-layer.sh
@@ -1,6 +1,12 @@
 mkdir python
-pip install -r ${2}/requirements.txt -t python/
-zip -r ${1}-layer.zip python/*
+pip install \
+    --platform manylinux2014_x86_64 \
+    --target=python/lib/python3.9/site-packages \
+    --implementation cp \
+    --python-version 3.9 \
+    --only-binary=:all: --upgrade \
+    -r ${2}/requirements.txt -t python/
+zip -r ${1}-layer.zip python
 aws s3 cp ${1}-layer.zip s3://quadball-os-packages/${1}.zip
 rm -r python
 rm ${1}-layer.zip
